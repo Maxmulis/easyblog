b47200a759e05e8b9792265d2d5dd89a9cd2e0f5
Max Keller <48801952+Maxmulis@users.noreply.github.com>
2022-07-10 22:03:41 +0200

    generate article model

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_10_193045) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_10_195059) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "articles", force: :cascade do |t|
+    t.string "title", null: false
+    t.text "text", null: false
+    t.date "date", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_articles_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username", null: false
@@ -22,4 +32,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_10_193045) do
     t.index ["username"], name: "index_users_on_username", unique: true
   end
 
+  add_foreign_key "articles", "users"
 end
